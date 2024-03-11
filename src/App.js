7030ea55edb998b18ebe537633e6e2e1e6fed21a
naresh <nareshcena666@gmail.com>
2024-03-11 23:36:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -21,15 +21,15 @@ function App() {
   return (
     <div className="App">
       <h1>Study Planner</h1>
-      <div className='inputs'>
-        <input id='course-name' type='text' />
-        <input id='duration' type='number' />
-        <button className='btn' id='addBtn' onClick={() => {clickHandler()}}>Add</button>
+      <div style={{display: 'flex', gap:'10px', justifyContent:'center'}} className='inputs'>
+        <input style={{height:'30px', width:'200px', padding:'0 10px', border:'2px solid gray', borderRadius:'8px'}} id='course-name' type='text' />
+        <input style={{height:'30px', width:'30px', textAlign:'center', border:'2px solid gray',borderRadius:'8px'}} id='duration' type='number' />
+        <button style={{ width:'60px', textAlign:'center', border:'none',borderRadius:'8px', backgroundColor:'#3B82F6', cursor:'pointer', color:'white'}} className='btn' id='addBtn' onClick={() => {clickHandler()}}>Add</button>
       </div>
-      <div style={{display: 'flex', gap: '50px' , flexWrap:'wrap', justifyContent: 'center', padding:'20px'  }}>
+      <div style={{display: 'flex', gap: '20px' ,flexDirection: 'column', flexWrap:'wrap', justifyContent: 'center', padding:'50px', alignItems:'center', color:'white'  }}>
       {courseData.map((course, index) => {
         return (
-          <div className='parentCard' style={{display: 'flex', gap: '10px' , flexWrap:'wrap'}}>
+          <div className='parentCard' style={{display: 'flex', gap: '10px' , flexWrap:'wrap', backgroundColor:'gray', padding:'10px', width: '40%', justifyContent: 'center', fontSize: '1.2rem', borderRadius: '8px', alignItems:'center' }}>
           <p>{index + 1}. </p>
           <CourseCard name={course.name} hour={course.time} key={index} />
           </ div>
--- src/CourseCard.js
+++ src/CourseCard.js
@@ -5,15 +5,19 @@ const CourseCard = ({name, hour, key}) => {
     let timeValue = parseInt(hourValue);
 
     return (
-      <div className='childCard' style={{display: 'flex', gap:'20px'}} key={key}>
-        <p>Subject: {name}</p>
-        <p>Duration: {hourValue} hrs</p>
-        <button onClick={() => {updateHourValue(timeValue + 1)}}>+</button>
-        <button onClick={() => {
+      <div className='childCard' style={{display: 'flex', gap:'20px', alignItems:'center'}} key={key}>
+        <p>{name}</p>
+        <p>-</p>
+        <div style={{display: 'flex', alignItems: 'center', gap:'10px'}}>
+        <button style={{cursor:'pointer', width: '30px', height:'30px', border: 'none', borderRadius: '100%', backgroundColor: 'red', color: 'white' }} onClick={() => {
             if(hourValue > 0 ){
                 updateHourValue(timeValue - 1)
             }
         }}>-</button>
+        <p style={{cursor:'pointer', color:'white'}}>{hourValue} <span style={{color:'black'}}>hrs</span></p>
+        <button style={{width: '30px', height:'30px', border: 'none', borderRadius: '100%', backgroundColor: 'green', color: 'white' }} onClick={() => {updateHourValue(timeValue + 1)}}>+</button>
+        </div>
+        
       </div>
     )
 }
